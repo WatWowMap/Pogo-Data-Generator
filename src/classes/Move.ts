14e105ca8914ae2e35c951e0aa5bdeaf16a2f21a
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2024-09-08 09:48:47 -0400

    chore: follow codebase style

--- src/classes/Move.ts
+++ src/classes/Move.ts
@@ -36,17 +36,21 @@ export default class Moves extends Masterfile {
       const id: number =
         Rpc.HoloPokemonMove[templateId.substring(11) as MoveProto]
       if (id || id === 0) {
-        let parsedMove = this.parsedMoves[id]
-        if (!parsedMove) parsedMove = this.parsedMoves[id] = {
-          moveId: id,
-          moveName: this.capitalize(templateId.substring(11).replace('_FAST', '')),
-          proto: templateId.substring(11),
-          fast: templateId.endsWith('_FAST'),
+        if (!this.parsedMoves[id]) {
+          this.parsedMoves[id] = {
+            moveId: id,
+            moveName: this.capitalize(
+              templateId.substring(11).replace('_FAST', ''),
+            ),
+            proto: templateId.substring(11),
+            fast: templateId.endsWith('_FAST'),
+          }
         }
-        parsedMove.type = Rpc.HoloPokemonType[moveSettings.pokemonType as TypeProto]
-        parsedMove.power = moveSettings.power
-        parsedMove.durationMs = moveSettings.durationMs
-        parsedMove.energyDelta = moveSettings.energyDelta
+        this.parsedMoves[id].type =
+          Rpc.HoloPokemonType[moveSettings.pokemonType as TypeProto]
+        this.parsedMoves[id].power = moveSettings.power
+        this.parsedMoves[id].durationMs = moveSettings.durationMs
+        this.parsedMoves[id].energyDelta = moveSettings.energyDelta
       }
     } catch (e) {
       console.warn(e, '\n', object)
@@ -62,18 +66,26 @@ export default class Moves extends Masterfile {
       const id: number =
         Rpc.HoloPokemonMove[templateId.substring(18) as MoveProto]
       if (id || id === 0) {
-        let parsedMove = this.parsedMoves[id]
-        if (!parsedMove) parsedMove = this.parsedMoves[id] = {
-          moveId: id,
-          moveName: this.capitalize(templateId.substring(18).replace('_FAST', '')),
-          proto: templateId.substring(18),
-          fast: templateId.endsWith('_FAST'),
+        if (!this.parsedMoves[id]) {
+          this.parsedMoves[id] = {
+            moveId: id,
+            moveName: this.capitalize(
+              templateId.substring(18).replace('_FAST', ''),
+            ),
+            proto: templateId.substring(18),
+            fast: templateId.endsWith('_FAST'),
+          }
         }
-        parsedMove.type = Rpc.HoloPokemonType[combatMove.type as TypeProto]
-        parsedMove.pvpPower = combatMove.power
-        if (combatMove.durationTurns) parsedMove.pvpDurationTurns = combatMove.durationTurns
-        parsedMove.pvpEnergyDelta = combatMove.energyDelta
-        if (combatMove.buffs) parsedMove.pvpBuffs = combatMove.buffs
+        this.parsedMoves[id].type =
+          Rpc.HoloPokemonType[combatMove.type as TypeProto]
+        this.parsedMoves[id].pvpPower = combatMove.power
+        this.parsedMoves[id].pvpEnergyDelta = combatMove.energyDelta
+        if (combatMove.durationTurns) {
+          this.parsedMoves[id].pvpDurationTurns = combatMove.durationTurns
+        }
+        if (combatMove.buffs) {
+          this.parsedMoves[id].pvpBuffs = combatMove.buffs
+        }
       }
     } catch (e) {
       console.warn(e, '\n', object)
