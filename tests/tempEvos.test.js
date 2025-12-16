355cc4b25b07123e8cf29e383a40c12cc09d7d48
Mygod <contact-git@mygod.be>
2025-12-15 22:56:58 -0800

    fix: test dup code

--- tests/tempEvos.test.js
+++ tests/tempEvos.test.js
@@ -1,3 +1,4 @@
+const { tempEvoIdKey } = require('../dist/utils/tempEvolutions')
 const tempEvos = require('../static/tempEvos.json')
 
 describe('static/tempEvos.json', () => {
@@ -7,7 +8,7 @@ describe('static/tempEvos.json', () => {
         const seen = new Set()
         const evolutions = (data && data.tempEvolutions) || []
         evolutions.forEach((evo) => {
-          const key = `${typeof evo.tempEvoId}:${evo.tempEvoId}`
+          const key = tempEvoIdKey(evo.tempEvoId)
           expect(seen.has(key)).toBe(false)
           seen.add(key)
         })
@@ -15,4 +16,3 @@ describe('static/tempEvos.json', () => {
     })
   })
 })
-
